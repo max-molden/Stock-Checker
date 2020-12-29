ecd6df39731d14dc0bf489a3b337c343d124d5e9
max-molden <max_molden@aol.com>
2020-12-28 20:28:00 -0500

    started writing function to handle ebay listings and give prices

--- 3070availability.py
+++ 3070availability.py
@@ -11,14 +11,16 @@ from pytz import reference # for time zone
 checkmark = "\u2714 " # checkmark
 red_x = "\u274C " # red heavy X
 yellow_star = "\u2B50" # yellow star
+dollar_sign = "\u0024"
 
 # specifically works for newegg as the HTML tags may be (and most likely are) different for different sites
 # a function to check if the item at the passed in url is in stock
 # Input: url (the specific url for an item; after it is clicked on not on the search page)
 # Processing: use requests and BeautifulSoup to get specific webpage info and check stock availability, then set a stock_bool var appropriately
 # Output: a stock_bool either T/F or an error message if somehting is weird
-def checkOnURLNewegg(url):
-    stock_bool = None # T/F value for if the item is in stock or not
+def checkOnUrlNewegg(url):
+    # T/F value for if the item is in stock or not
+    stock_bool = None
 
     # get the webpage with requests and make a soup from it with BeautifulSoup
     webpage = rq.get(url)
@@ -45,17 +47,34 @@ def checkOnURLNewegg(url):
 
     return stock_bool
 
-# a function to display whether or not a specific listing is in stock, does not matter the website, provides link for easy copy/paste if want
-# Input: stock_bool (T/F item in stock or not), url(link to site for ease of access), description (simple description of brand and anything else before RTX 3070, plus that as well)
+def checkOnUrlEBay(url):
+    # float to store the price of the listing
+    list_price = 0
+
+    # get the webpage with requests and make a soup from it with BeautifulSoup
+    webpage = rq.get(url)
+    soup = BeautifulSoup(webpage.content, "lxml")
+
+    x = "WHAT DO NOW?"
+    "IM STOPPING HERE FOR NOW, until tommorrow probs"
+
+# a function to display whether or not a specific listing is in stock, provides link for easy copy/paste if want
+# Input: stock_bool (T/F item in stock or not), url(link to site for ease of access), description (simple description of brand and anything else before RTX 3070, plus that as well), output_file (the file to be written to)
 # Processing: super simple if/else using the stock_bool as condition for the if.  If in stock display checkmark and delimeter (globals).  Both display the 
 #   Else display X mark.  Both display whether item is or is NOT ins tock along with a link underneath for ease of access
-# Output: writes to a file
+# Output: writes to a file the information provided in a formatted manner
 def displayStock(stock_bool, url, description, output_file):
-
     if stock_bool:
         output_file.write(f"{yellow_star*20}\n{checkmark}The {description} is in stock.\n\t\tLink: {url}\n{yellow_star*20}\n\n")
     else:
         output_file.write(f"{red_x}The {description} is NOT in stock.\n\t\tLink: {url}\n\n")
+
+# a function to display the price of a specific listing on eBay
+# Input: price (float from web scraping ebay func), url (link to site for ease of access), description (simple description of brand and anything else before RTX 3070, plus that as well), output_file (the file to be written to)
+# Processing: just literally plugs all the info into an f-string
+# Output: writes to a file the information provided in a formatted manner
+def displayPrice(price, url, description, output_file):
+    output_file.write(f"{dollar_sign}The {description} is listed at ${price}.\n\n")
 
 '''
 ALWAYS MAKE SURE TO HIT ENTER AT THE END OF EACH LINE, INCLUDING THE LAST
@@ -123,6 +142,7 @@ def main():
     # vars to help with printing respective site names, false if we haven't printed, true if have
     #   i.e. it will check if we have printed "new egg sites" and if it has then it will not print it agian, then will print best buy listings, etc.
     newegg_bool = False
+    ebay_bool = False
 
     # go over all the row info, passing, respectively, the correct info to the displayStock() and checkonURLxxx() funcs
     for row in range(0, len(infos)):
@@ -130,10 +150,13 @@ def main():
             if not newegg_bool:
                 output_file.write("NEWEGG LISTINGS\n\n")
                 newegg_bool = not newegg_bool
-            # calls displayStock(checkonURLNewegg(url), url, descriptor) since infos is a 2D array where each element is an array containig the sitename, url, and the descriptor as its 3 elements
-            displayStock(checkOnURLNewegg(infos[row][1]),  infos[row][1], infos[row][2], output_file)
-        else: # else it is anotehr site, call appropriate func
-            pass
+            # calls displayStock(checkonUrlNewegg(url), url, descriptor) since infos is a 2D array where each element is an array containig the sitename, url, and the descriptor as its 3 elements
+            displayStock(checkOnUrlNewegg(infos[row][1]),  infos[row][1], infos[row][2], output_file)
+        elif infos[row][0] == "ebay": # if it is an ebay site, I want to list the price
+            if not ebay_bool:
+                output_file.write("EBAY LISTINGS")
+                ebay_bool = not ebay_bool
+            displayPrice()
     
     output_file.close() # close file just to be safe, good practice
 
